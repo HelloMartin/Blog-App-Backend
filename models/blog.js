bd8c2e97f022a91ccc9b9e2043326d325e0b88c4
Martin Vogt <info@hellomartin.ch>
2019-03-18 16:52:41 +0100

    Subtitle for Blogs

--- models/blog.js
+++ models/blog.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
     title: String,
+    subtitle: String,
     image: String,
     body: String,
     created: {type: Date, default: Date.now}
